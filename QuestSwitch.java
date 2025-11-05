c865b5bae3063e895ee5cd93f42b3cb510f232b6
qiqifahreza <bungbeng002@gmail.com>
2025-11-05 14:24:36 +0700

    menambahkan syarat mendaftar sebagai asisten dosen dan pemilihan matkul

--- QuestSwitch.java
+++ QuestSwitch.java
@@ -10,10 +10,12 @@ public class QuestSwitch {
             int bData;
             int algodPem;
             int rpLunak;
+            int pilihan;
+            int nPilihan = 0;
             double tNilai;
             double rRata;
             double rRIPK;
-            String predikat;
+            String predikat, mk1, mk2, mk3, mk4, mk5, matKul = null;
 
         System.out.println("masukkan nilai struktur data:");
             sData = nScan.nextInt();
@@ -53,6 +55,43 @@ public class QuestSwitch {
 
             System.out.println("predikat nilai adalah: " + predikat);
             System.out.println("rata-rata IPK nilai adalah: " + rRIPK);
+            if (predikat.equals("A (Sangat Baik)") || predikat.equals("AB (Baik Sekali)")) {
+                System.out.println("Anda memenuhi syarat untuk mendaftar sebagai Asisten Dosen.");
+                System.out.println("Pilih mata kuliah yang ingin diajukan sebagai Asisten Dosen:");
+                System.out.println("\n 1. Struktur Data \n 2. Pemrograman  Berorientasi Objek \n 3. Basis Data \n 4. Algoritma dan Pemrograman \n 5. Rekayasa Perangkat Lunak" );
+                mk1 = "Struktur Data";
+                mk2 = "Pemrograman  Berorientasi Objek";
+                mk3 = "Basis Data";
+                mk4 = "Algoritma dan Pemrograman";
+                mk5 = "Rekayasa Perangkat Lunak";
+                
+                pilihan = nScan.nextInt();
+                switch (pilihan) {
+                    case 1:
+                        nPilihan = sData;
+                        matKul = mk1;
+                        break;
+                    case 2:
+                        nPilihan = pbObjek;
+                        matKul = mk2;
+                        break;
+                    case 3:
+                        nPilihan = bData;
+                        matKul = mk3;
+                        break;
+                    case 4:
+                        nPilihan = algodPem;
+                        matKul = mk4;
+                        break;
+                    case 5:
+                        nPilihan = rpLunak;
+                        matKul = mk5;
+                        break;
+                    default:
+                        System.out.println("Pilihan tidak valid.");
+                        System.exit(0);
+                }
         
+        }
     }
 }
